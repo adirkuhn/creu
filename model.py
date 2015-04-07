e6822600a737b256f2bdaa72c0106a5c074fb095
Adir Kuhn <adir.kuhn@abagrobrasil.com.br>
2015-04-07 14:20:47 -0300

    ajuste cron

--- model.py
+++ model.py
@@ -9,6 +9,7 @@ from sqlalchemy.ext.declarative import declarative_base
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+app.config.from_pyfile('flaskapp.cfg')
 app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 db = SQLAlchemy(app)
